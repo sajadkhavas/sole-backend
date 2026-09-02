377ca5a0450544f07b94c70151a14542cd20325e
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-09-02 10:31:14 +0330

    style(p08): remove unused controller import

--- app/Http/Controllers/Api/V1/TrustSupportController.php
+++ app/Http/Controllers/Api/V1/TrustSupportController.php
@@ -15,7 +15,6 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
-use RuntimeException;
 
 class TrustSupportController extends Controller
 {
